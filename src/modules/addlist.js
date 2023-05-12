51c8f4f95600cea9a5780dba766f280a6c06036c
iamalex <ngashalex8@gmail.com>
2023-05-12 12:10:09 +0300

    added edit button, created local storage

--- src/modules/addlist.js
+++ src/modules/addlist.js
@@ -1,50 +1,71 @@
 import clearCheckedItems from './delete.js';
+import editTask from './Update.js';
+import saveData from './saveData.js';
 
 const listItems = document.getElementById('list-container');
-
 const InputBox = document.getElementById('input-box');
-
 const clearBtn = document.getElementById('close-btn');
-
 const addTask = document.getElementById('addTask');
-
-const saveData = () => {
-  localStorage.setItem('data', listItems.innerHTML);
-};
 
 function addTask1() {
   if (InputBox.value === '') {
-    alert('You must write something');
+    alert('input cannot be blank');
   } else {
     const li = document.createElement('li');
-    li.draggable = true;
     li.innerHTML = InputBox.value;
-    listItems.appendChild(li);
 
     const span = document.createElement('span');
-    span.innerHTML = '\'<img src="images/trash-can.png" alt="trash">\'';
     span.innerHTML = '\u00d7';
+    span.className = 'close';
+    span.onclick = () => {
+      const taskList = JSON.parse(localStorage.getItem('tasks'));
+      const taskIndex = Array.prototype.indexOf.call(listItems.children, li);
+      taskList.splice(taskIndex, 1);
+      localStorage.setItem('tasks', JSON.stringify(taskList));
+      li.remove();
+    };
+
+    const editIcon = document.createElement('i');
+    editIcon.className = 'fa fa-edit';
+    editIcon.onclick = () => {
+      editTask(editIcon);
+    };
+
+    li.appendChild(editIcon);
     li.appendChild(span);
+
+    listItems.appendChild(li);
+
+    const task = {
+      description: InputBox.value,
+      completed: false,
+    };
+
+    const taskList = JSON.parse(localStorage.getItem('tasks')) || [];
+    taskList.push(task);
+    localStorage.setItem('tasks', JSON.stringify(taskList));
   }
 
   InputBox.value = '';
-  saveData();
 }
 
 addTask.addEventListener('click', addTask1);
-
-listItems.addEventListener('click', (e) => {
-  if (e.target.tagName === 'LI') {
-    e.target.classList.toggle('checked');
-    saveData();
-  } else if (e.target.tagName === 'SPAN') {
-    e.target.parentElement.remove();
-    saveData();
-  }
-}, false);
+listItems.addEventListener(
+  'click',
+  (e) => {
+    if (e.target.tagName === 'LI') {
+      e.target.classList.toggle('checked');
+      saveData();
+    } else if (e.target.tagName === 'SPAN') {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false,
+);
 
 const showTask = () => {
-  listItems.innerHTML = localStorage.getItem('data');
+  InputBox.innerHTML = localStorage.getItem('data');
 };
 showTask();
 
@@ -56,7 +77,6 @@ function clearAll(e) {
 }
 
 clearBtn.addEventListener('click', clearAll);
-
 clearBtn.addEventListener('click', () => {
   clearCheckedItems();
 });
